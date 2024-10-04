58186db343136f38c00b0fc002bb832798cc34a0
Anastasia Bromberg <137691009+AABrom@users.noreply.github.com>
2024-10-04 13:59:23 +0300

    Add files via upload

---
+++ easy_scripts/simple_or_not.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+import 'dart:math';
+
+void main(){
+    var a = int.parse(stdin.readLineSync() ?? '0');
+    simple(a);
+}
+
+void simple(n){ 
+    if (n<2){
+        return print('NO');
+    }
+	for(var i=2; i<=sqrt(n);i++){
+		if(n%i==0){
+			return print('NO');
+        }
+    }    
+	return print('YES');
+}
